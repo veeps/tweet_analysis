c26e04f1ee1457537245192fdc85bc51af03ee8b
veeps <vpeng12@gmail.com>
2020-06-16 16:12:59 -0700

    set up project files. Removing python notebook for data collection. Converted to python script

---
+++ src/scraper/tweet_scraper.py
@@ -0,0 +1,22 @@
+import GetOldTweets3 as got
+import pandas as pd
+
+def pull_tweets(username, count, start_date, end_date):
+	"""
+	Pull old tweets
+	username (string) = Twitter username
+	count (numeric) = Number of tweets to pull. Max 3000
+	start_date (string) = YYYY-MM-DD
+	end_date (string) = YYYY-MM-DD
+	"""
+
+    # Creation of query object
+	tweetCriteria = got.manager.TweetCriteria().setUsername(username).setSince(start_date).setUntil(end_date).setMaxTweets(count)
+
+    # Creation of list that contains all tweets
+	tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+
+    # Create df for tweets
+	user_tweets = pd.DataFrame([[tweet.date, tweet.text] for tweet in tweets])
+    
+	return user_tweets
