84c0cc303e6b1342485a8fe36d59e94adde9ce14
veeps <vpeng12@gmail.com>
2020-06-16 22:50:20 -0700

    exploring tweets with keywords

--- src/scraper/words.py
+++ src/scraper/words.py
@@ -3,9 +3,27 @@ from sklearn.feature_extraction import text
 import pandas as pd
 
 def top_words(vectorizer, tweets):
-    df = pd.DataFrame(vectorizer.fit_transform(tweets).toarray(), columns=vectorizer.get_feature_names())
+	"""
+	Selects the top 30 words 
+	vectorizer = type of vectorizer
+	tweets = list of tweets to parse
+	"""
+	df = pd.DataFrame(vectorizer.fit_transform(tweets).toarray(), columns=vectorizer.get_feature_names())
     
     # create dataframe with top words
-    top_words = df.sum().T.sort_values(0, ascending=False).head(30)
+	top_words = df.sum().T.sort_values(0, ascending=False).head(30)
     
-    return top_words
+	return top_words
+
+
+def select_tweets(df, column, keywords):
+	"""
+	Filters dataframe based on a list of keywords
+
+	df = dataframe
+	column = tweets column
+	keywords = list of keywords
+	"""
+	df = df[df[column].str.contains('|'.join(keywords), regex=True)]
+
+	return df 
