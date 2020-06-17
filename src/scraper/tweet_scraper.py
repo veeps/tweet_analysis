cf2040011ff2ebcb15cd0e09576b9cce3189b660
veeps <vpeng12@gmail.com>
2020-06-16 17:05:27 -0700

    moved cleaning function to tweet_scraper.py module. Deleted data cleaning notebook. Deleted previous Twitter pulls that separated into dates

--- src/scraper/tweet_scraper.py
+++ src/scraper/tweet_scraper.py
@@ -1,5 +1,8 @@
 import GetOldTweets3 as got
 import pandas as pd
+import regex as re
+import string
+
 
 def pull_tweets(username, start_date, end_date):
 	"""
@@ -20,3 +23,55 @@ def pull_tweets(username, start_date, end_date):
 	user_tweets = pd.DataFrame([[tweet.date, tweet.text] for tweet in tweets])
     
 	return user_tweets
+
+
+def clean_tweets(df, tweets):
+	"""
+	Clean text column
+	df = dataframe
+	tweets (string) = column name containing tweets
+	"""
+	# lowercase text
+	df[tweets] = df[tweets].str.lower()
+
+	# remove URLs
+	df[tweets] = df[tweets].map(lambda x: re.sub('http[s]?:\/\/[^\s]*', ' ', x))
+
+	# remove URL cutoffs
+	df[tweets] = df[tweets].map(lambda x: re.sub('\\[^\s]*', ' ', x))
+
+	# remove spaces
+	df[tweets] = df[tweets].map(lambda x: re.sub('\n', ' ', x))
+
+	# remove picture URLs
+	df[tweets] = df[tweets].map(lambda x: re.sub('pic.twitter.com\/[^\s]*', ' ', x))
+
+	# remove blog/map type
+	df[tweets] = df[tweets].map(lambda x: re.sub('blog\/maps\/info\/[^\s]*', ' ', x))
+
+	# remove hashtags =
+	df[tweets] = df[tweets].map(lambda x: re.sub("\#[\w]*", "", x))
+
+	# remove AT users
+	df[tweets] = df[tweets].map(lambda x: re.sub("\@[\w]*", "", x))
+
+	# remove single quotations
+	df[tweets] = df[tweets].map(lambda x: re.sub("'", "", x))
+	df[tweets] = df[tweets].map(lambda x: re.sub("'", "", x))
+
+	# remove characters that are not word characters or digits
+	df[tweets] = df[tweets].map(lambda x: re.sub("[^\w\d]", " ", x))
+
+	# remove all characters that are not letters
+	df[tweets] = df[tweets].map(lambda x: re.sub("[^a-zA-Z]", " ", x))
+
+	# remove multiple spaces
+	df[tweets] = df[tweets].map(lambda x: re.sub("\s{2,6}", " ", x))
+
+	# drop duplicate rows
+	df.drop_duplicates(subset='text', keep='first', inplace=True)
+
+	# remove multiple spaces
+	df[tweets] = df[tweets].map(lambda x: re.sub("\s{3,20}", "", x))
+
+	return df
